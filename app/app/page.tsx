2737367221f659d26183b9585475cec607e6ac79
opkhardware0-coder <opkhardware0@gmail.com>
2026-09-14 17:57:48 +0000

    Delete app directory

--- app/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { requireAuth } from '@/lib/supabase-server';
-import App from '@/components/App';
-
-export const dynamic = 'force-dynamic';
-
-export default async function Page() {
-  const profile = await requireAuth();
-  return <App profile={profile} />;
-}
