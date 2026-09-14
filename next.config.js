e7ca821d282ae93261605e878e8bea9d734d7a5d
opkhardware0-coder <opkhardware0@gmail.com>
2026-09-14 17:18:34 +0000

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['your-supabase-project-id.supabase.co'], // Replace with your Supabase project ID
-  },
-}
-
-module.exports = nextConfig
